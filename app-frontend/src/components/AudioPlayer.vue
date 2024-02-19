300faf094bb630367596a23746c01371f729a451
Calvin Farrell <farr8480@ravnes.benedictine.edu>
2024-02-19 16:22:33 -0600

    Merge conflict resolutions that caused accidental deletions and deleting CWPlayer (temporary file for testing).

--- app-frontend/src/components/AudioPlayer.vue
+++ app-frontend/src/components/AudioPlayer.vue
@@ -1,5 +1,3 @@
-<!-- import * as jscwlib from src/jscwlib.js; -->
-
 <template>
     <input
         type="range"
@@ -33,20 +31,17 @@
 <script lang = "ts" setup>
 import { onMounted, ref, watch, type Ref, computed, type ComputedRef } from "vue"
 import { jscw } from "./jscwlib.js"
+import { setTextRange } from "typescript";
 
-const AudioPlayerExample: Ref<HTMLAudioElement | null> = ref(null)
+const AudioPlayerExample: Ref<HTMLAudioElement | null> = ref(null);
 
-<<<<<<< HEAD
-const audioctx = new Audio
-=======
-const player = ref(null)
+const player = ref(null);
 
-function setup_jscw(){
+function setup_jscw() {
     jscw_var.setWpm(30);
-    jscw_var.setText("hello"); // We should have some reparate component that allows us to pass the text in as a param
-    jscw_var.renderPlayer(player.value, jscw_var); // replace this with custom Vue UI
+    jscw_var.setText("hello");
+    jscw_var.renderPlayer(player.value, jscw_var);
 }
->>>>>>> b04a45b4a28d82d4de7797243a69ed98eeca13f6
 
 onMounted(() => {
     setup_jscw();
@@ -91,73 +86,5 @@ onMounted(() => {
         // onSeek() {
         //     const seekto = this.duration * (this.seekValue / 100);
         //     this.currentTime = seekto;
-        // },
-    // function genMorseTiming(string character, bigint currentTime) {
-    //     var out = [];
-    //     var 1 = getDitDah(character);
-    //     if (!1) {
-    //         console.log("CW for character" + character +" unavailable");
-    //     }
-    // }
-    // function getDitDah(character) {
-
-    // }
-    
-    // class Alphabet {
-    //     private englishAlphabet = new Alphabet[2][26];
-    //     constructor(){
-    //         this.englishAlphabet[0][0] = "A";
-    //         this.englishAlphabet[0][1] = "B";
-    //         this.englishAlphabet[0][2] = "C";
-    //         this.englishAlphabet[0][3] = "D";
-    //         this.englishAlphabet[0][4] = "E";
-    //         this.englishAlphabet[0][5] = "F";
-    //         this.englishAlphabet[0][6] = "G";
-    //         this.englishAlphabet[0][7] = "H";
-    //         this.englishAlphabet[0][8] = "I";
-    //         this.englishAlphabet[0][9] = "J";
-    //         this.englishAlphabet[0][10] = "K";
-    //         this.englishAlphabet[0][11] = "L";
-    //         this.englishAlphabet[0][12] = "M";
-    //         this.englishAlphabet[0][13] = "N";
-    //         this.englishAlphabet[0][14] = "O";
-    //         this.englishAlphabet[0][15] = "P";
-    //         this.englishAlphabet[0][16] = "Q";
-    //         this.englishAlphabet[0][17] = "R";
-    //         this.englishAlphabet[0][18] = "S";
-    //         this.englishAlphabet[0][19] = "T";
-    //         this.englishAlphabet[0][20] = "U";
-    //         this.englishAlphabet[0][21] = "V";
-    //         this.englishAlphabet[0][22] = "W";
-    //         this.englishAlphabet[0][23] = "X";
-    //         this.englishAlphabet[0][24] = "Y";
-    //         this.englishAlphabet[0][25] = "Z";
-    //         this.englishAlphabet[1][0] = ".-";
-    //         this.englishAlphabet[1][1] = "-..";
-    //         this.englishAlphabet[1][2] = "-.-.";
-    //         this.englishAlphabet[1][3] = "";
-    //         this.englishAlphabet[1][4] = "E";
-    //         this.englishAlphabet[1][5] = "F";
-    //         this.englishAlphabet[1][6] = "G";
-    //         this.englishAlphabet[1][7] = "H";
-    //         this.englishAlphabet[1][8] = "I";
-    //         this.englishAlphabet[1][9] = "J";
-    //         this.englishAlphabet[1][10] = "K";
-    //         this.englishAlphabet[1][11] = "L";
-    //         this.englishAlphabet[1][12] = "M";
-    //         this.englishAlphabet[1][13] = "N";
-    //         this.englishAlphabet[1][14] = "O";
-    //         this.englishAlphabet[1][15] = "P";
-    //         this.englishAlphabet[1][16] = "Q";
-    //         this.englishAlphabet[1][17] = "R";
-    //         this.englishAlphabet[1][18] = "S";
-    //         this.englishAlphabet[1][19] = "T";
-    //         this.englishAlphabet[1][20] = "U";
-    //         this.englishAlphabet[1][21] = "V";
-    //         this.englishAlphabet[1][22] = "W";
-    //         this.englishAlphabet[1][23] = "X";
-    //         this.englishAlphabet[1][24] = "Y";
-    //         this.englishAlphabet[1][25] = "Z";
-    //     }
-    //  }
+        // }
 </script>
