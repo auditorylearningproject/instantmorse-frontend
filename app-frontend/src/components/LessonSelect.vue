b8de7b934fcafb01f8c0e9f537a0ac13836c46b2
DavidG <dgarst2001@gmail.com>
2024-03-25 17:11:58 -0500

    Lesson Select Format

--- app-frontend/src/components/LessonSelect.vue
+++ app-frontend/src/components/LessonSelect.vue
@@ -8,12 +8,25 @@
             </form>
     </div>
     <div>
-        <select v-model='fetchLesson'>
-            <option disabled value="">Select a Lesson</option>
-            <option v-for="lesson in lessons" :key="lesson.id" value="">{{ lesson.lesson_name }}</option>
+        <select v-model="selectedItem" @change="fetchData">
+            <option v-for="lesson in dropdownOptions" :value="lesson">{{ lesson }}</option>
         </select>
+        <ul>
+            <li v-for="lesson in lessons" :key="lesson._id">
+                {{ lesson.lesson_name }}
+                {{ lesson.array_o_chars }}
+            </li>
+        </ul>
+        <!-- <select v-model="fetchLesson"> -->
+        <!-- <v-select :options="lessons" label="name">
+            <template #option="option">
+                <span>{{ option.name }}</span>
+            </template>
+            <option disabled value="">Select a Lesson</option>
+            <option v-for="lesson in lessons" :key="lesson.id" value="{{ lesson.name }}">{{ lesson.name + ": " +  lesson.content }}</option> -->
+        <!-- </v-select> -->
         <form @submit.prevent='selecting'>
-            <button class='selectionMade' @click='selecting'>Go to This Lesson</button> <br><br>
+            <button class='selectionMade'>Go to This Lesson</button> <br><br>
         </form>
     </div>
     </template>
@@ -22,20 +35,19 @@
 
 <script lang="ts">
     import axios from 'axios';
+    // import { ref } from 'vue'
+
     export default {
         data() {
             return {
-                lessons : []
+                selectedItem: null,
+                dropdownOptions: ['Option 1', 'Option 2', 'Option 3'], // Populate dropdown options
+                lessons : [] as { _id: number, lesson_name: string, array_o_chars: string, group: object }[]
             };
         },
-        fetchLesson() {
-            console.log("Popping open a selection");
-            try {
-                axios.get(location.origin+"/api/lesson/select")
-                .then(response => this.lessons = response.data);
-            } catch(error) {
-                console.error("Error switching to lesson page", error);
-            }
+        mounted() {
+            // Fetch initial data
+            this.fetchData();
         },
         methods: {
             async goToHome() {
@@ -44,6 +56,21 @@
                     this.$router.push('/');
                 } catch(error) {
                     console.error("Error switching to home page", error);
+                }
+            },
+            async fetchData() {
+                console.log("Popping open a selection");
+                try {
+                    // Make sure a dropdown item is selected
+                    if (!this.selectedItem) return;
+
+                    // Make a request to backend with selected item
+                    await axios.get(`/api/lesson/select?dropdownItem=${this.selectedItem}`)
+                    .then(response => this.lessons = response.data);
+                    // axios.get(location.origin+"/api/lesson/select")
+                    // .then(response => this.lessons = response.data);
+                } catch(error) {
+                    console.error("Error switching to lesson page", error);
                 }
             },
             async selecting() {
@@ -61,8 +88,16 @@
 </script>
 
 <style scoped>
+    @import "../../node_modules/vue-select/dist/vue-select.css";
+
 header {
     height: 70px;
     text-align: center;
 }
+
+option.name {
+    text-decoration-color: rgb(0, 0, 0);
+}
+
+
 </style>
