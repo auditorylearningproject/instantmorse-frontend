07a5760bfb1486c9cf4052882220a95faadf3e3b
DavidG <dgarst2001@gmail.com>
2024-04-16 00:09:39 -0500

    Lesson Page Works

--- app-frontend/src/views/LessonSelect.vue
+++ app-frontend/src/views/LessonSelect.vue
@@ -21,55 +21,56 @@
 <!-- https://www.bezkoder.com/vue-node-express-mongodb-mevn-crud/ -->
 
 <script setup lang="ts">
-    import axios from 'axios';
-import { onMounted, ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router'
-import NavigationHeader from '../components/NavigationHeader.vue';
-
-
-const router = useRouter()
-    // import { ref } from 'vue'
-interface Lesson {
-  _id: number;
-  lesson_name: string;
-  array_o_chars: string[];
-  group: object;
-}
-
-        let lessons: Lesson[] = [{_id: -1, lesson_name: "Please wait...", array_o_chars: [], group: {} }];
-
-        const selectedItem = ref()
-
-        onMounted(() => {
-            fetchData();
-        });
-
-        const goToHome = async () => {
-                console.log("Ladies and Gentlemen, we got him");
-                try {
-                    router.push('/');
-                } catch(error) {
-                    console.error("Error switching to home page", error);
-                }
-            };
-            const fetchData = async () => {
-                console.log("Popping open a selection");
-                try {
-                    await axios.get('/api/lesson/select')
-                    .then(response => lessons = response.data);
-                } catch(error) {
-                    console.error("Error switching to lesson page", error);
-                }
-            };
-            const selecting = async () => {
-                try {
-                    router.push('/lesson');
-                } catch(error) {
-                    console.error("Error switching to lesson page", error);
-                }
-            };
     import 'vue-select/dist/vue-select.css'
-    
+    import axios from 'axios';
+    import { onMounted, ref } from 'vue';
+    import { useRoute, useRouter } from 'vue-router'
+    import NavigationHeader from '../components/NavigationHeader.vue';
+
+
+    const router = useRouter()
+        // import { ref } from 'vue'
+    interface Lesson {
+    _id: number;
+    lesson_name: string;
+    array_o_chars: string[];
+    group: object;
+    }
+
+    let lessons: Lesson[] = [{_id: -1, lesson_name: "Please wait...", array_o_chars: [], group: {} }];
+
+    const selectedItem = ref()
+
+    onMounted(() => {
+        fetchData();
+    });
+
+    let selectedItemThing = "";
+    const goToHome = async () => {
+        console.log("Ladies and Gentlemen, we got him");
+        try {
+            router.push('/');
+        } catch(error) {
+            console.error("Error switching to home page", error);
+        }
+    };
+    const fetchData = async () => {
+        console.log("Popping open a selection");
+        try {
+            await axios.get('/api/lesson/select')
+            .then(response => lessons = response.data);
+        } catch(error) {
+            console.error("Error switching to lesson page", error);
+        }
+    }; 
+    const selecting = async () => {
+        try {
+            selectedItemThing = selectedItem.value._id;
+            router.push('/lesson/'+selectedItemThing);
+        } catch(error) {
+            console.error("Error switching to lesson page", error);
+        }
+    };
 </script>
 
 <style scoped>
