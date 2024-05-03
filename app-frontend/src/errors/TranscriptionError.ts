e2a6798e0b8c937113cabf8e8943666f985d2832
Luke Aschenbrenner <ashbren21@gmail.com>
2024-05-03 12:28:54 -0500

    Fix catching transcription error and fix issue with the loop looping too fast and audio slices not getting decoded

--- app-frontend/src/errors/TranscriptionError.ts
+++ app-frontend/src/errors/TranscriptionError.ts
@@ -1,4 +1,4 @@
-class TranscriptionError extends Error {
+export class TranscriptionError extends Error {
     constructor(message: string) {
       super(message);
       this.name = 'TranscriptionError';
